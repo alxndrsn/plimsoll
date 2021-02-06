22132e2cb86c2583c3a742b9eaa9a40a7018c2a2
Alex Anderson <alxndrsn>
2021-02-06 08:26:10 +0000

    Add Stryker Mutator

---
+++ test/stryker.conf.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  packageManager: 'yarn',
+  reporters: ['html', 'clear-text', 'progress'],
+  testRunner: 'mocha',
+  coverageAnalysis: 'perTest',
+  mutate: [ 'src/plimsoll.js' ],
+  tempDirName: 'build/.stryker-tmp',
+};
